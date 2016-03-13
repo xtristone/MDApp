994ac8b9feb7788cf84f2a44de2e8fd2e27579d5
tristone <xtristone@gmail.com>
2016-03-13 10:31:58 +0800

    添加自定义View

---
+++ app/src/main/java/com/example/mei/mdapp/MaskView.java
@@ -0,0 +1,21 @@
+package com.example.mei.mdapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/3/13.
+ */
+public class MaskView extends View{
+
+
+    public MaskView(Context context) {
+        super(context);
+    }
+
+    public MaskView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
